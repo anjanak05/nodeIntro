be281c41a7784dc71c62b665ef011b473bae52f1
anjanak05 <anjna4523@gmail.com>
2022-10-09 10:21:34 +0530

    calculator done

--- day-1/calculator/index.js
+++ day-1/calculator/index.js
@@ -1,3 +1,13 @@
 const {addition, division, subtraction, mulitplication} = require("../calculator/calculater")
 
-console.log(mulitplication(2,3))
+// console.log(mulitplication(2,3))
+const process = require('process');
+
+const ans1 = subtraction(+process.argv[3], +process.argv[4]);
+const ans2 = division(+process.argv[3], +process.argv[4]);
+const ans3 = mulitplication(+process.argv[3], +process.argv[4]);
+const ans4 = addition(+process.argv[3], +process.argv[4]);
+console.log(ans1)
+console.log(ans2)
+console.log(ans3)
+console.log(ans4)
