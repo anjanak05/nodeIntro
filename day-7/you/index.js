7ae82854fb00069b757a6a9fdc116c6c78db290e
anjanak05 <anjna4523@gmail.com>
2022-10-30 23:59:32 +0530

    auth done

--- day-7/you/index.js
+++ day-7/you/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { connection } = require("./config/db.js");
-const {signUpModal} = require("./models/signup.model.js")
+const { signUpModal } = require("./models/signup.model.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
 
 const app = express();
 
@@ -10,20 +12,46 @@ app.get("/", (req, res) => {
   res.send("HOME PAGE");
 });
 
-app.post("/signup", async (req, res) => {
-  console.log(req.body);
-  try {
-    const signupData = new signUpModal(req.body);
-    signupData.save();
-    res.send("registered successfully"); 
-} catch (err) {
-    console.log(err);
+app.get("/dashboard", (req, res) => {
+const token = req.headers.authorization.split(" ")[1]
+// console.log(token)
+  try{var decoded = jwt.verify(token, 'abc12345');
+      console.log(decoded)
+  res.send(`welcome ${decoded.email} DAshboard Page`)}
+  catch(err){
+    console.log(err)
+    res.send("PLease login again")
   }
- 
 });
 
-app.post("/login", (req, res) => {
-  res.send("loged in successfully");
+app.post("/signup", async (req, res) => {
+  const {email, password} = req.body;
+      bcrypt.hash(password, 4, function(err, hash) {
+      if(err){
+        res.send("something went wrong, try gaian later")
+      }else{
+        const signupData = new signUpModal({email, password:hash});
+        signupData.save();
+        res.send("registered successfully"); 
+      }
+
+  });
+    
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPass = await signUpModal.findOne({email})
+ 
+  bcrypt.compare(password, hashedPass.password, function(err, result) {
+    if (result) {
+      const token = jwt.sign({email:email}, "abc12345" , { expiresIn: '7h' });
+      res.send(`logged in with ${email} `);
+    } else {
+      res.send("Failed");
+    }
+});
+  
 });
 
 app.listen(8000, async () => {
